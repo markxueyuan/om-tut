60e0d7049a226e0cec779f2808940e748a2135bb
markxueyuan <ziyaajiang@gmail.com>
2014-06-20 18:03:15 +0800

    init

---
+++ src/om_tut/core.cljs
@@ -0,0 +1,14 @@
+(ns om-tut.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+(def app-state (atom {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+
+(om/root
+  (fn [app owner]
+    (apply dom/ul nil
+      (map (fn [text] (dom/li nil text) (:list app)))))
+  app-state
+  {:target (. js/document (getElementById "app0"))})
